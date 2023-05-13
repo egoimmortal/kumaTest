690db0576ca2ea95c0fd2605c3a9c95fcf6e9080
egoimmortal <want0945@gmail.com>
2023-05-14 03:05:29 +0800

    修改色系跳轉回首頁

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,11 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView
   },
   {
+    path: '/color',
+    name: 'color',
+    redirect: '/'
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
