c0cacfb990368618370e2227c4f4fff2bbfe3aa4
egoimmortal <want0945@gmail.com>
2023-05-14 17:48:46 +0800

    修改包版設定

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV == 'development' ? './' : '/kumaTest/'
+  publicPath: process.env.NODE_ENV == 'development' ? './' : '/kumaTest/',
+  configureWebpack: {
+    devServer: {
+      historyApiFallback: true
+    }
+  }
 })
