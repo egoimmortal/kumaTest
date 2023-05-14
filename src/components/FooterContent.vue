fa1f2289eb2700fda55501b23d5f5f7637ed28b0
egoimmortal <want0945@gmail.com>
2023-05-14 16:41:44 +0800

    新增abort頁面，新增內容footer

---
+++ src/components/FooterContent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="footer">
+        <span>
+            酷碼科技
+        </span>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+@media (max-width: 576px) {
+    .footer span{
+        text-align: center;
+    }
+}
+@media (min-width: 577px) {
+    .footer span{
+        text-align: start;
+    }
+}
+.footer{
+    position: absolute;
+    bottom: 0px;
+    color: #E0E0E0;
+    background-color: var(--content-footer);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    span{
+        width: 90%;
+        
+    }
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<AbortCom class="content"></AbortCom>
+		<FooterContent class="footer"></FooterContent>
+	</div>
 </template>
+
+<script setup lang="ts">
+import AbortCom from '@/components/AbortCom.vue';
+import FooterContent from '@/components/FooterContent.vue';
+</script>
+
+<style lang="scss" scoped>
+.about{
+	& .content{
+		width: 100%;
+		height: 92%;
+	}
+
+	& .footer{
+		width: 100%;
+		height: 8%;
+	}
+}
+</style>
